3e611317ff9beb5812f172444e3391c2f3fa628d
Dre <1418145349@qq.com>
2019-08-15 15:04:07 +0800

    frest 1

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')();
+const http = require('http').createServer(express);
+const io = require('socket.io')(http);
+
+io.on('connection',client=>{
+    console.log('a user connected :D');
+    client.on('chat message',msg=>{
+        io.emit('chat message',msg);
+    })
+    client.on('disconnect',err=>{
+        console.log("user disconnect :D");
+    })
+})
+
+http.listen(3000,err=>{
+    if(err) throw err;
+    console.log("server runing on port 3000");
+})
